399b390929dc157fd509654444291c408fb4ba1d
Zarar2357 <aminzarar@gmail.com>
2025-02-22 10:24:55 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,4 +1,4 @@
-//writing a program to check if the number is even or odd
+# writing a program to check if the number is even or odd
 x=input('Enter the number: ')
 if x%2==0:
     print('The number is even')
