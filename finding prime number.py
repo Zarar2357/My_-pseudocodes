fb3465aa237e2ea1b11c86abd3395110c30ff4f8
Zarar2357 <aminzarar@gmail.com>
2025-02-22 10:20:38 +0530

    Update and rename finding prime number_pseudo.pseudo to finding prime number.py

--- finding prime number.py
+++ finding prime number.py
@@ -2,8 +2,8 @@
 input: number1
 n=round(sqrt(number1))
 for i in range(2,n+1):
-if number1%i == 0
+if number1%i == 0:
     output: "Not Prime"
-    exit
+    exit()
 output: "Prime"if number mod i ==0
 
