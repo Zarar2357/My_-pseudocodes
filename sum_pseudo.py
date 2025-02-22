5a8456f4769171b9ef340d48d42a03e6d3c72cc8
Zarar2357 <aminzarar@gmail.com>
2025-02-22 10:22:38 +0530

    Update sum_pseudo.py

--- sum_pseudo.py
+++ sum_pseudo.py
@@ -1,4 +1,4 @@
-// writing a program to find the sum of two numbers
+# writing a program to find the sum of two numbers
 x= input("Enter the first number: ")
 y= input("Enter the second number: ")
 sum= x+y
